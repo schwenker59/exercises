c93cd8dc648dde561873bf99774b5ea4f9e419c7
Chris Ortman <chris-ortman@uiowa.edu>
2017-09-27 21:34:02 -0500

    Exercises for chapter 3

---
+++ chapter-3/exercise-3-4.py
@@ -0,0 +1,31 @@
+# Programming Exercise 3-4
+#
+# Program to convert a number from 1 to 10 to a Roman numeral.
+# This program will accept an integer from 1 to 10 from the user
+# and use it to choose an appropriate Roman numeral
+# to display on the screen.
+
+# Variables to hold a number and a numeral.
+# initialize the number as an int and the numeral as a string.
+
+# Get number from user and convert it to an int
+
+
+# Set numeral to a Roman numeral value based on the value of number
+# use a set of if ... elif .... etc. statements.
+
+
+
+
+
+
+# use a final else to display an error if number is out of range.
+
+
+# display the numeral on the screen.
+
+
+
+
+
+
---
+++ chapter-3/exercise-3-5.py
@@ -0,0 +1,32 @@
+# Programming Exercise 3-5
+#
+# Program to convert a value in kilograms to Newtons, then check whether it is in range.
+# This program will prompt a user for a mass in kilograms,
+# convert it to Newtons and use constants to check if the weight is within range,
+# then display the weight and a range message.
+
+
+
+# Global constants for minimum, maximum and mass multiplier values
+
+
+# Variables for weight and mass initialized as floats   
+
+
+# Get mass from user and convert it to a float
+
+
+# Calculate weight using the mass multiplier constant
+
+
+# Display the weight
+
+# If weight > maximum or < than minimum display an appropriate message
+
+
+
+
+
+
+
+
