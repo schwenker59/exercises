50eadb01d7589bd3151d834e7425c64a4cabd352
Chris Ortman <chris-ortman@uiowa.edu>
2017-10-03 22:09:40 -0500

    Chapter 4 exercises

---
+++ chapter-4/ex-4-5.py
@@ -0,0 +1,36 @@
+# Programming Exercise 4-5
+#
+# Program to compute total and average monthly rainfall over a span of years.
+# This program gets a number of years from a user,
+# then uses nested loops to prompt for rainfall for each month in each year
+#	and calculate the total and the average monthly rainfall,
+# then displays the number of months, total rainfall and average monthly rainfall
+
+# Create float variables for total rainfall, monthly rainfall, average monthly rainfall
+
+# Create int variables for number of years and number of months.
+
+
+
+# Get number of years from the user
+
+# Nested loop logic to loop through the years and their months
+#
+# Outer for loop for the number of years
+
+	# Print the current year message
+
+	# Inner loop for 12 months 
+
+		# Get monthly rainfall for current month from the user
+		
+		# add monthly rainfall to total rainfall
+		
+		# increment number of months
+		
+			
+# Calculate the average rainfall using total rainfall and number of months
+
+# print the results on the screen, including details for total months, total rainfall,
+#	and average monthly rainfall, formatting any floats to 2 decimal places.
+
---
+++ chapter-4/ex-4-6.py
@@ -0,0 +1,30 @@
+# Programming Exercise 4-6
+#
+# Program to display a table of Celsius and Fahrenheit equivalents.
+# This program takes no input.
+# It calculates a series of Fahrenheit equivalents for a range of Celsius temperatures
+# then displays them in a table.
+
+
+# Declare a float variable to hold the Fahrenheit temperature.
+
+
+# Declare a constant to hold the max celsius value
+
+
+# Display a table header with Celsius and Fahrenheit separated by two tabs.
+# Display a line separator made of underscores
+
+
+
+# Use a for loop to iterate from 0 through the range of Celsius temperatures
+
+
+# 	Calculate the Fahrenheit temperature for the current Celsius value
+
+#	Display the current Celsius and Fahrenheit values, separated by two tabs
+
+
+
+
+
