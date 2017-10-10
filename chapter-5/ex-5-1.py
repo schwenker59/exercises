b082675c3010fb103b42615adfaae34e9de045d4
Chris Ortman <chris-ortman@uiowa.edu>
2017-10-09 22:51:36 -0500

    Add chapter 5

---
+++ chapter-5/ex-5-1.py
@@ -0,0 +1,36 @@
+# Programming Exercise 5-1
+#
+# Program to convert kilometers to miles.
+# This program accepts a distance in kilometers from the user,
+# passes it to a function, which calculates its value in miles
+# and displays the result for the user.
+
+
+# Global constant for the ratio of kilometers to miles
+
+
+# define the main function
+
+    # Define a local float variable to hold the distance in kilometers
+
+    # Get distance in kilometers from the user
+
+    # pass the distance in kilometers to a function to convert to miles
+
+
+
+# define the function to convert to miles
+# the function takes kilometers as an argument
+# calculates the equivalent number of miles
+# and prints the result
+
+    # Define a local float variable for miles
+
+	# use the global conversion constant to compute miles    
+    
+    # print the results, formatting float values to 2 decimal places
+
+
+
+# Call the main function to start the program
+
---
+++ chapter-5/ex-5-3.py
@@ -0,0 +1,42 @@
+# Programming Exercise 5-3
+#
+# Program to compute recommended insurance to carry on an item.
+# This program accepts a replacement cost from a user,
+# calculates a minimum recommended insurance to carry from a global constant,
+# then passes these values to a function to be displayed
+
+
+
+# Global constant for the percent of replacement cost to insure
+
+
+
+# Define the main function
+
+    # Define local float variables for replacement cost and minimum insurance
+
+
+    # Get the replacement cost from the user
+
+    # Calculate the minimum insurance amount using the global constant
+
+    # Call the function to display the insurance details, 
+    # passing the replacement cost and minimum insurance to the function
+
+
+      
+# Define a function to display the insurance details
+# This function accepts the replacement cost and minimum insurance as parameters,
+# uses the global constant to calculate percent insured,
+# and displays the insurance details.
+
+	# display the replacement cost, formatting the value to 2 decimal places
+
+    # display the percent insured, formatting the value to 2 decimal places
+
+	# display the minimum insurance, formatting the value to 2 decimal places
+
+
+
+# Call the main function to start the program
+
